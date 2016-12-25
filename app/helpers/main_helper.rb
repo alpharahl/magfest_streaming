85a3bd81dcf65d6692990614a82e83927bbe1d81
alpha <alpha@alphas-MacBook-Pro.local>
2016-12-25 01:10:39 -0500

    Display of panel data is now working

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,9 +1,4 @@
 module MainHelper
-	require 'open-uri'
-	require 'json'
 
-	def get_json_of_panels
-		JSON.parse(open("https://super2017.uber.magfest.org/uber/schedule/panels_json").read)
-	end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,34 @@
 class Room < ActiveRecord::Base
+	def get_current(time)
+	    Panel.where(room_id: self.id).each do |p|
+	        if p.start_unix < time.to_i and p.end_unix > time.to_i
+	        	return p
+	        end
+	    end
+	    return nil # If no panel was found, then return nil
+	end
+
+	def get_next(time)
+		Panel.where(room_id: self.id).order(start_unix: :asc).each do |p|
+			if p.start_unix < time.to_i
+				return p
+			end
+		end
+		return nil
+	end
+
+
+	def display_current(time)
+	    panel = get_current(time)
+	    if panel
+	        return "#{panel.name} <br>   #{panel.start[0..panel.start.index(" ")]} - #{panel.end[0..panel.end.index(" ")]}".html_safe	
+	    else
+	    	next_panel = get_next(time)
+	    	if next_panel == nil
+	    		return "Finished"
+	    	else
+	    		return "Break, Resuming at #{next_panel.start[0..next_panel.start.index(" ")]} <br> #{next_panel.name}".html_safe
+	    	end
+	    end
+	end
 end

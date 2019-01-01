cd7663373c1552d7be3c7366c61e291bacff24a7
alpharahl <david.m.gauntlett@gmail.com>
2019-01-01 18:05:05 -0500

    allow prod to host static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,6 @@ config.webpacker.check_yarn_integrity = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier

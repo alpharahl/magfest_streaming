3526ee12dee72899d07deea5f7349d5e1321ee2a
alpharahl <david.m.gauntlett@gmail.com>
2019-01-01 17:58:57 -0500

    allow prod to host static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ config.webpacker.check_yarn_integrity = false
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.serve_static_assets = true
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like

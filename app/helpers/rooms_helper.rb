f72b4d4b4cf96d3c7b6be8aea07e82ca8334bc0d
alpharahl <david.m.gauntlett@gmail.com>
2019-01-03 13:45:37 -0500

    setting timezone

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -3,8 +3,8 @@ module RoomsHelper
     current_time = ENV['STUB_TIME'].nil? ? Time.now : Time.parse(ENV['STUB_TIME'])
     panels_list = DataPopulater.get_json_of_panels
     panels_list.delete_if{|panel|
-      panel["start_unix"] > (current_time + 5.hours + 15.minutes).to_i or
-      panel["end_unix"] < (current_time + 5.hours).to_i
+      panel["start_unix"] > (current_time + 15.minutes).to_i or
+      panel["end_unix"] < (current_time).to_i
     }
     # Force order by start time
     panels_list.sort!{|a, b|
